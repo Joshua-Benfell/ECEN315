8969344c7359758da02c85f537545223eb0854b6
Joshua-Benfell <joshuabenfell@hotmail.co.nz>
2020-05-11 20:57:47 +1200

    Update q6.m

--- assignment2/q6.m
+++ assignment2/q6.m
@@ -2,16 +2,20 @@ clear
 clf
 
 G = tf(6, [1 7 6 0]);
-%C = pidtool(G, 'p');
-%After manually tuning with pidtool, this value was the first that gave no
-%overshoot.
-%C = 0.252510468359574;
-%C = 0.229638
-C = 7.01;
+C = [4, 5, 6, 7, 8];
+
+Legend = cell(length(C), 1);
+i = 1;
 figure(1)
-subplot(1,2,1)
-sys = feedback(C * G, 1)
-step(sys, 200);
-subplot(1,2,2)
-pzmap(sys)
-damp(sys)
+hold on
+for a = C
+   sys = feedback(series(a, G), 1);
+   step(sys, 40);
+   Legend{i} = strcat(num2str(a), 'V Step');
+   i = i + 1;
+end
+hold off
+legend(Legend)
+
+figure(2)
+step(feedback(series(-0.1,  G), 1), 40);
