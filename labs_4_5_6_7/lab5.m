83fea5593c97eb4d1305c3b083a8283402608e6f
Joshua-Benfell <joshuabenfell@hotmail.co.nz>
2020-06-03 20:55:05 +1200

    Lab 5 code

--- labs_4_5_6_7/lab5.m
+++ labs_4_5_6_7/lab5.m
@@ -1,10 +1,13 @@
 clear
 
 sys = tf([0,0,0,0,360.030121478794], [1,11.5758154704500,84.2362155121299,470.992803495151,1178.09505338830])%From Lab 3
+sys = 1/4 * sys * 180/pi;
 feedbackSys = feedback(sys,1)
-pzmap(feedbackSys)
-step(feedbackSys)
+figure(1)
+%pzmap(feedbackSys)
+%step(feedbackSys)
 hold on 
-step(sys)
+step(20*sys)
 hold off
+figure(2)
 rlocus(sys)
