3527d203f185320fdeb3db48c9e7eeed5284b9b8
Joshua-Benfell <joshuabenfell@hotmail.co.nz>
2020-06-26 23:03:00 +1200

    tri 1 done

--- labs_4_5_6_7/Lab5/lab5.m
+++ labs_4_5_6_7/Lab5/lab5.m
@@ -1,29 +1,41 @@
 clear
+close all
 
 sys = tf([0,0,0,0,360.030121478794], [1,11.5758154704500,84.2362155121299,470.992803495151,1178.09505338830])%From Lab 3
-sys =  sys * 1/4 * 0.228
+sys =  sys * 1/4 * 0.228 * 180/pi
 
-kp = 0.5;
-kd = -0.5;
+kp = 0.38;
+ki = 1.87;
+kd = -0.04;
 %ki = 2.81;
 %I = tf([1 ki/kp], 1);
 %G = tf(1, [1 0]);pi/180
 s = tf('s');
 %compensated = kp * I * G * sys;
-compensated = (s*kd + kp) * sys;
+compensated = (kd * s + kp + ki/s) * sys;
 feedbackSys = feedback(compensated,1);
 %figure(1)
 %t = 0.01: 0.01: 10;
 %pzmap(feedbackSys)
-%step(20 * feedbackSys * pi/180 )
+figure(4)
+step(20 * feedbackSys);
+figure(5)
+bode(sys)
+hold on
+bode(compensated)
+bode((0.1*s + kp) * sys);
+hold off
+legend("Uncompensated", "PD Compensated K_D = -0.1", "PD Compensated K_p = 0.1")
 %hold on 
 %step(20 * pi/180 * sys)
 %hold off1/
 %figure(2)
 figure(1)
 rlocus(sys)
-figure(2)
+hold on
 rlocus(compensated)
+legend("Uncompensated", "PD Compensated")
+hold off
 figure(3)
 rlocus(feedbackSys)
 
--- labs_4_5_6_7/Lab6/simulinkIterate.m
+++ labs_4_5_6_7/Lab6/simulinkIterate.m
@@ -1,14 +1,29 @@
 clear
+close all
 time = 10;
 simFile = 'lab6_sec_5__PID';
 PIDSystem = 'lab6_sec_5__PID/CompensatedSystem';
 load_system(simFile);
-set_param(PIDSystem, 'Kp', '1');
-%figure(1)
-%hold on
-%for i = 1:0.2:2
-%    set_param(PIDSystem, 'Kp', num2str(i));
-%    sim(simFile, time);
-%    plot(tout, pendulumStepResponse);
-%end
-linearSystemAnalyzer(PIDSystem)
+set_param(PIDSystem, 'Kp', '0.505');
+set_param(PIDSystem, 'Ki', '0');
+set_param(PIDSystem, 'Kd', '0');
+figure(1)
+hold on
+range = -0.3:0.1:0.3;
+txt = cell(length(range), 1);
+allStepInfo= cell(length(range), 1);
+count = 1;
+for i = range
+    txt{count} = strcat("K_d: ", num2str(i));
+    set_param(PIDSystem, 'Kd', num2str(i));
+    sim(simFile, time);
+    plot(tout, pendulumStepResponse);
+    allStepInfo{count} = stepinfo(pendulumStepResponse,tout);
+    count = count + 1;    
+end
+
+hold off
+legend(txt)
+title('Step Response to various K_d Values and Input of 20^o')
+ylabel('Angular Displacement')
+xlabel('Time (s)')
